adb2a9d962e0ec814e7345485fa5263a19b8de9c
Gabriel Santos <33208964+GabrielSantos23@users.noreply.github.com>
2022-12-19 15:43:59 -0300

    Add files via upload

---
+++ server/Controllers/AuthContriller.js
@@ -0,0 +1,65 @@
+import UserModel from '../Modals/userModal.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// Registering a new user
+
+export const registerUser = async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+  const newUser = new UserModel(req.body);
+  const { username } = req.body;
+  try {
+    const oldUser = await UserModel.findOne({ username });
+
+    if (oldUser) {
+      return res.status(400).json({ message: 'username is already in use' });
+    }
+
+    const user = await newUser.save();
+
+    const token = jwt.sign(
+      {
+        username: user.username,
+        id: user._id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: '24h' }
+    );
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// login User
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+
+    if (user) {
+      const valiity = await bcrypt.compare(password, user.password);
+
+      if (!valiity) {
+        res.status(400).json('Wrong password');
+      } else {
+        const token = jwt.sign(
+          {
+            username: user.username,
+            id: user._id,
+          },
+          process.env.JWT_KEY,
+          { expiresIn: '1h' }
+        );
+        res.status(200).json({ user, token });
+      }
+    } else {
+      res.status(404).json('User does not exist');
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ server/Routes/PostRoute.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getTimelinePosts,
+  likePost,
+  updatePost,
+  getAllPosts,
+} from '../Controllers/PostController.js';
+const router = express.Router();
+
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.put('/:id/like', likePost);
+router.get('/:id/timeline', getTimelinePosts);
+router.get('/:id/posts', getAllPosts);
+
+export default router;
